d286f62330ebfdb810e69f15b38a3ba5d85061e2
carlossan1978 <50367557+carlossan1978@users.noreply.github.com>
2019-10-01 06:33:50 -0500

    casa

---
+++ RTVSS/Lafrancol.Solu_Eventos.Persistencia/Models/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lafrancol.Solu_Eventos.Persistencia.Models
+{
+    public class Evento
+    {
+        public long idEvento { get; set; }
+        public string nombre { get; set; }
+        public DateTime fecha { get; set; }
+        public string ciudad { get; set; }
+        public string descripcion { get; set; }
+        public bool estado { get; set; }
+        
+    }
+}
---
+++ RTVSS/Lafrancol.Solu_Eventos.Persistencia/Models/Participante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lafrancol.Solu_Eventos.Persistencia.Models
+{
+    public class Participante
+    {
+        public long idParticipante { get; set; }
+        public string nombre { get; set; }
+        public virtual Evento Evento { get; set; }
+    }
+}
